f27e516289d3ae5925112b30e6ba77d3d17188a7
Robin Roussel <robin.roussel.15@ucl.ac.uk>
2021-06-28 17:30:26 +0100

    Modify HTML and JS files to work with Flask
    And take advantage of this to load the initial floorplans dynamically

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,15 +1,3 @@
-const floorplanPaths = [
-    './data/0a6e347e-51ac-453c-85d0-c569e44c7a41.geojson',
-    './data/1b3b292a-c737-4314-9302-9ffe4233d5be.geojson',
-    './data/2fa534fb-2028-4113-98d4-b9ed5d36e08b.geojson',
-    './data/51620c8e-32a5-4ed1-aed5-286b09621208.geojson',
-    './data/57e7103c-50bc-4002-a96c-f10058c991a9.geojson',
-    './data/6d322c6a-b617-4e6f-809b-d0609861c1b3.geojson',
-    './data/7a56de60-01f6-440c-8565-0fb420fffe18.geojson',
-    './data/a4b26891-0b70-49fe-8dc0-7425ce903c4a.geojson',
-    './data/c867cb96-c666-49cd-869d-a16ce43a3164.geojson',
-    './data/fffe2a5b-bec2-494a-97d9-e53c75c0cea6.geojson'
-];
 const bigMapOptions = {
     // Sets the right scaling options for fitBounds()
     crs: L.CRS.Simple,
@@ -17,6 +5,7 @@ const bigMapOptions = {
     minZoom: -3,
     attributionControl: false,
 };
+
 const mapOptions = {
     // Sets the right scaling options for fitBounds()
     crs: L.CRS.Simple,
@@ -34,6 +23,7 @@ const mapOptions = {
     // trackResize: false,
     zoomControl: false
 };
+
 const labelColormap = {
     'door.arc': '#000000',
     'door.frame': '#000000',
@@ -50,6 +40,7 @@ const labelColormap = {
     'window': '#000000',
     '': '#ffffff'
 };
+
 const setPlanStyle = function(feature) {
     return {
         'color': 'black',
@@ -57,6 +48,7 @@ const setPlanStyle = function(feature) {
         'fillColor': labelColormap[feature.properties.label]
     };
 };
+
 const setPopUp = function(feature, layer) {
     if (feature.properties.label) {
         const labelParts = feature.properties.label.split('.');
@@ -65,6 +57,7 @@ const setPopUp = function(feature, layer) {
         }
     }
 };
+
 const dataDiv = document.querySelector('.data');
 // Create the big plan view.
 const bigPlanContainer = document.querySelector('.big-plan-container');
@@ -74,27 +67,38 @@ document.querySelector('.close-button').onclick = function() {
     bigPlanMap.eachLayer(layer => bigPlanMap.removeLayer(layer));
     bigPlanContainer.classList.add('invisible');
 };
-// Create the array of plan thumbnails.
-for (path of floorplanPaths) {
-    fetch(path)
-        .then(response => response.json())
-        .then(plan => {
-            const planDiv = document.createElement('div');
-            planDiv.className = 'plan';
-            dataDiv.appendChild(planDiv);
-            const map = L.map(planDiv, mapOptions);
-            const layer = L.geoJSON(plan, {style: setPlanStyle});
-            layer.addTo(map);
-            map.fitBounds(layer.getBounds(), {padding: [10, 10]});
-            planDiv.onclick = function() {
-                if (bigPlanContainer.classList.contains('invisible')) {
-                    const bigPlanLayer = L.geoJSON(
-                        plan, {style: setPlanStyle, onEachFeature: setPopUp}
-                    );
-                    bigPlanLayer.addTo(bigPlanMap);
-                    bigPlanMap.fitBounds(bigPlanLayer.getBounds(), {padding: [50, 50]});
-                    bigPlanContainer.classList.remove('invisible');
-                }
-            };
-        });
-}
+
+const displayFloorPlans = function(plans) {
+    for (let plan of plans) {
+        const planDiv = document.createElement('div');
+        planDiv.className = 'plan';
+        dataDiv.appendChild(planDiv);
+        const map = L.map(planDiv, mapOptions);
+        const layer = L.geoJSON(plan, {style: setPlanStyle});
+        layer.addTo(map);
+        map.fitBounds(layer.getBounds(), {padding: [10, 10]});
+        planDiv.onclick = function() {
+            if (bigPlanContainer.classList.contains('invisible')) {
+                const bigPlanLayer = L.geoJSON(
+                    plan, {style: setPlanStyle, onEachFeature: setPopUp}
+                );
+                bigPlanLayer.addTo(bigPlanMap);
+                bigPlanMap.fitBounds(
+                    bigPlanLayer.getBounds(), {padding: [50, 50]}
+                );
+                bigPlanContainer.classList.remove('invisible');
+            }
+        };
+    }
+};
+
+const searchFloorPlans = function(/*query*/) {
+    return fetch("/FloorPlanSearch", {
+        // method: 'GET',
+        // body: JSON.stringify(query),
+        // headers:{'Content-Type': 'application/json'}
+    }).then(response => response.json());
+};
+
+searchFloorPlans().then(plans => displayFloorPlans(plans));
+// searchFloorPlans().then(plans => console.log(plans));
